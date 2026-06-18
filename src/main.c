5481def690f95fde24a0cbfc74c05b97aebbf37e
Andrey Polivoda <apolivodaa433@gmail.com>
2026-06-19 01:19:44 +1000

    Remove unneeded #defines

--- src/main.c
+++ src/main.c
@@ -9,48 +9,6 @@
 #include <string_init.h>
 #include <system_tables_setup.h>
 
-// Interesting Skylake events
-// From https://github.com/intel/perfmon/blob/main/SKL/events/skylake_core.json
-// Format: bits 15-8: UMASK, bits 7-0: EVENT_CODE
-#define UOPS_EXECUTED_THREAD 0x0181
-#define UOPS_EXECUTED_CORE 0x02b1
-#define UOPS_EXECUTED_X87 0x10b1
-#define UOPS_ISSUED_ANY 0x010e
-#define UOPS_ISSUED_VECTOR_WIDTH_MISMATCH 0x020e
-#define UOPS_ISSUED_SLOW_LEA 0x200e
-
-#define UOPS_DISPATCHED_PORT_0 0x01a1
-#define UOPS_DISPATCHED_PORT_1 0x02a1
-#define UOPS_DISPATCHED_PORT_2 0x04a1
-/// and so on... umask = (1<<port_num)
-
-#define UOPS_RETIRED_RETIRE_SLOTS 0x02c2
-#define UOPS_RETIRED_MACRO_FUSED 0x04c2
-
-#define IDQ_MS_UOPS 0x3079
-#define IDQ_DBS_UOPS 0x0879
-#define IDQ_MITE_UOPS 0x0479
-
-#define OTHER_ASSISTS_ANY 0x3fc1
-#define FP_ASSIST_ANY 0x1eca
-
-#define DSB2MITE_SWITCHES_COUNT 0x01ab
-
-#define INST_DECODED_DECODERS 0x0155
-
-#define INST_RETIRED_ANY_P 0x00c0
-#define INST_RETIRED_ANY 0x0100
-#define INST_RETIRED_NOP 0x02c0
-
-#define BR_INST_RETIRED_NOT_TAKEN 0x10c4
-
-#define MEM_INST_RETIRED_ALL_LOADS 0x81d0
-#define MEM_INST_RETIRED_ALL_STORES 0x82d0
-#define MEM_INST_RETIRED_ANY 0x83d0
-
-#define MEM_INST_RETIRED_SPLIT_LOADS 0x41d0
-#define MEM_INST_RETIRED_SPLIT_STORES 0x42d0
-
 #define MSR_IA32_PMC0 0xc1
 
 #define MSR_IA32_PERFEVTSEL0 0x186
